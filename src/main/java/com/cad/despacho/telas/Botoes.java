7dd932516a97b79b063b8192910ebc5056e577c3
Xryonz <i0560008@gmail.com>
2026-08-24 14:08:37 -0300

    cria um helper pra montar botao de selecao (toggle) sem repetir a configuracao toda vez

---
+++ src/main/java/com/cad/despacho/telas/Botoes.java
@@ -0,0 +1,20 @@
+package com.cad.despacho.telas;
+
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+
+public class Botoes {
+
+    public static ToggleButton criarToggle(String texto, ToggleGroup grupo, String classeCss) {
+
+        ToggleButton botao = new ToggleButton(texto);
+        botao.getStyleClass().add(classeCss);
+        botao.setToggleGroup(grupo);
+        botao.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(botao, Priority.ALWAYS);
+
+        return botao;
+    }
+}
