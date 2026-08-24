d05b9754bfd3bb2562297a0fc91086a4e4292fa1
Xryonz <i0560008@gmail.com>
2026-08-24 14:20:54 -0300

    comunicacao sem estilo, e a listview/treeview usam a celula padrao agora

--- src/main/java/com/cad/despacho/telas/TelaComunicacao.java
+++ src/main/java/com/cad/despacho/telas/TelaComunicacao.java
@@ -1,9 +1,7 @@
 package com.cad.despacho.telas;
 
 import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.HBox;
@@ -35,17 +33,14 @@ public class TelaComunicacao {
     VBox montarViaturasAtivas() {
 
         VBox painel = new VBox(16);
-        painel.getStyleClass().add("card");
         painel.setPrefWidth(280);
         painel.setMinWidth(280);
 
         Label titulo = new Label("VIATURAS ATIVAS");
-        titulo.getStyleClass().add("card-title");
 
         ListView<String> lista = new ListView<>();
         lista.getItems().addAll("ABTR-05", "ASU-12", "UR-03");
-        lista.setCellFactory(lv -> criarCelulaCodigo());
-        lista.setFixedCellSize(38);
+        lista.setFixedCellSize(28);
         lista.prefHeightProperty().bind(lista.fixedCellSizeProperty().multiply(lista.getItems().size()).add(2));
 
         painel.getChildren().add(titulo);
@@ -53,30 +48,11 @@ public class TelaComunicacao {
         return painel;
     }
 
-    ListCell<String> criarCelulaCodigo() {
-        return new ListCell<>() {
-            @Override
-            protected void updateItem(String item, boolean empty) {
-                super.updateItem(item, empty);
-                getStyleClass().remove("item-codigo");
-                if (empty || item == null) {
-                    setText(null);
-                } else {
-                    setText(item);
-                    getStyleClass().add("item-codigo");
-                }
-            }
-        };
-    }
-
     VBox montarRecursos() {
 
         VBox painel = new VBox(16);
-        painel.getStyleClass().add("card");
 
         Label titulo = new Label("DISPOSICAO DE RECURSOS");
-        titulo.getStyleClass().add("card-title");
-        titulo.getStyleClass().add("mono");
 
         TreeItem<String> raizArvore = new TreeItem<>("");
         raizArvore.getChildren().add(criarCategoria("CANAIS DE RADIO ATIVOS", "A-2", "B-1", "C-4", "F-12"));
@@ -84,8 +60,7 @@ public class TelaComunicacao {
 
         TreeView<String> arvore = new TreeView<>(raizArvore);
         arvore.setShowRoot(false);
-        arvore.setCellFactory(tv -> criarCelulaArvore());
-        arvore.setFixedCellSize(32);
+        arvore.setFixedCellSize(28);
         arvore.prefHeightProperty().bind(arvore.fixedCellSizeProperty().multiply(arvore.expandedItemCountProperty()).add(2));
 
         painel.getChildren().add(titulo);
@@ -101,25 +76,5 @@ public class TelaComunicacao {
             categoria.getChildren().add(new TreeItem<>(item));
         }
         return categoria;
-    }
-
-    TreeCell<String> criarCelulaArvore() {
-        return new TreeCell<>() {
-            @Override
-            protected void updateItem(String item, boolean empty) {
-                super.updateItem(item, empty);
-                getStyleClass().removeAll("arvore-categoria", "arvore-item");
-                if (empty || item == null) {
-                    setText(null);
-                } else {
-                    setText(item);
-                    if (getTreeItem() != null && !getTreeItem().isLeaf()) {
-                        getStyleClass().add("arvore-categoria");
-                    } else {
-                        getStyleClass().add("arvore-item");
-                    }
-                }
-            }
-        };
     }
 }
