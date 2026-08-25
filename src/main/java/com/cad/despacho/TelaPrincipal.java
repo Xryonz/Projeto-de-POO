5344768b28afa56647705d44360f79c409fcd090
Xryonz <i0560008@gmail.com>
2026-08-25 13:21:28 -0300

    tela principal ficou bem menor, so monta o menu, nao precisa mais trocar conteudo

--- src/main/java/com/cad/despacho/TelaPrincipal.java
+++ src/main/java/com/cad/despacho/TelaPrincipal.java
@@ -1,41 +1,16 @@
 package com.cad.despacho;
 
-import javafx.geometry.Insets;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 
 public class TelaPrincipal {
-
-    StackPane areaConteudo = new StackPane();
-    MenuLateral menu = new MenuLateral();
 
     public BorderPane criar() {
 
         BorderPane raiz = new BorderPane();
-        raiz.setPadding(new Insets(32));
 
-        VBox menuPronto = menu.criar(this);
-        raiz.setLeft(menuPronto);
-        BorderPane.setMargin(menuPronto, new Insets(0, 24, 0, 0));
-
-        raiz.setCenter(areaConteudo);
-
-        navegar("nova");
+        MenuLateral menu = new MenuLateral();
+        raiz.setLeft(menu.criar());
 
         return raiz;
-    }
-
-    public void navegar(String tela) {
-
-        areaConteudo.getChildren().clear();
-
-        if (tela.equals("nova")) {
-            areaConteudo.getChildren().add(new TelaNovaOcorrencia().criar(this));
-        } else if (tela.equals("consultar")) {
-            areaConteudo.getChildren().add(new TelaConsultarOcorrencias().criar());
-        } else if (tela.equals("comunicacao")) {
-            areaConteudo.getChildren().add(new TelaComunicacao().criar());
-        }
     }
 }
