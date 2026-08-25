9417263de5c2aa3bd312147a08db81f268288721
Xryonz <i0560008@gmail.com>
2026-08-25 13:21:21 -0300

    nova ocorrencia: tirei combobox, checkbox e campo de texto, virou tudo label e botao, sem espacamento

--- src/main/java/com/cad/despacho/TelaNovaOcorrencia.java
+++ src/main/java/com/cad/despacho/TelaNovaOcorrencia.java
@@ -1,96 +1,63 @@
 package com.cad.despacho;
 
-import java.util.Set;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 public class TelaNovaOcorrencia {
 
-    public VBox criar(TelaPrincipal pai) {
+    public VBox criar() {
 
-        VBox raiz = new VBox(24);
+        VBox raiz = new VBox();
         raiz.getChildren().add(new Label("REGISTRAR NOVA OCORRENCIA"));
 
-        VBox card = new VBox(20);
+        VBox card = new VBox();
 
         Label tituloCard = new Label("DADOS DO EVENTO / SINISTRO");
 
         Label labelTipo = new Label("TIPO DE OCORRENCIA");
-        ComboBox<String> comboTipo = new ComboBox<>();
-        comboTipo.getItems().addAll("Incendio Comercial", "Incendio Residencial", "Incendio Florestal",
-                "Colisao de Veiculos", "Atendimento Clinico");
-        comboTipo.setValue("Incendio Comercial");
-        VBox campoTipoBox = new VBox(8);
+        Label valorTipo = new Label("Incendio Comercial");
+        VBox campoTipoBox = new VBox();
         campoTipoBox.getChildren().add(labelTipo);
-        campoTipoBox.getChildren().add(comboTipo);
-        HBox.setHgrow(campoTipoBox, Priority.ALWAYS);
+        campoTipoBox.getChildren().add(valorTipo);
 
         Label labelPrioridade = new Label("PRIORIDADE TATICA");
-        ToggleGroup grupoPrioridade = new ToggleGroup();
-        ToggleButton botaoAlta = new ToggleButton("ALTA");
-        botaoAlta.setToggleGroup(grupoPrioridade);
-        HBox.setHgrow(botaoAlta, Priority.ALWAYS);
-        botaoAlta.setSelected(true);
-        ToggleButton botaoMedia = new ToggleButton("MEDIA");
-        botaoMedia.setToggleGroup(grupoPrioridade);
-        HBox.setHgrow(botaoMedia, Priority.ALWAYS);
-        ToggleButton botaoBaixa = new ToggleButton("BAIXA");
-        botaoBaixa.setToggleGroup(grupoPrioridade);
-        HBox.setHgrow(botaoBaixa, Priority.ALWAYS);
-        HBox boxPrioridade = new HBox(8, botaoAlta, botaoMedia, botaoBaixa);
-        VBox campoPrioridadeBox = new VBox(8);
+        Button botaoAlta = new Button("ALTA");
+        Button botaoMedia = new Button("MEDIA");
+        Button botaoBaixa = new Button("BAIXA");
+        HBox boxPrioridade = new HBox(botaoAlta, botaoMedia, botaoBaixa);
+        VBox campoPrioridadeBox = new VBox();
         campoPrioridadeBox.getChildren().add(labelPrioridade);
         campoPrioridadeBox.getChildren().add(boxPrioridade);
-        HBox.setHgrow(campoPrioridadeBox, Priority.ALWAYS);
 
-        HBox linha1 = new HBox(16, campoTipoBox, campoPrioridadeBox);
+        HBox linha1 = new HBox(campoTipoBox, campoPrioridadeBox);
 
         Label labelLocal = new Label("LOCALIZACAO");
-        TextField campoLocal = new TextField();
-        campoLocal.setPromptText("Rua, numero - Bairro, Cidade/UF");
-        VBox campoLocalBox = new VBox(8);
+        Label valorLocal = new Label("Rua, numero - Bairro, Cidade/UF");
+        VBox campoLocalBox = new VBox();
         campoLocalBox.getChildren().add(labelLocal);
-        campoLocalBox.getChildren().add(campoLocal);
+        campoLocalBox.getChildren().add(valorLocal);
 
         Label labelData = new Label("DATA E HORA DO REGISTRO");
-        TextField campoDataHora = new TextField();
-        campoDataHora.setText("09/05/2026 - 14:15");
-        campoDataHora.setEditable(false);
-        VBox campoDataBox = new VBox(8);
+        Label valorData = new Label("09/05/2026 - 14:15");
+        VBox campoDataBox = new VBox();
         campoDataBox.getChildren().add(labelData);
-        campoDataBox.getChildren().add(campoDataHora);
-        campoDataBox.setPrefWidth(220);
+        campoDataBox.getChildren().add(valorData);
 
         Label labelSolicitante = new Label("SOLICITANTE / CONTATO");
-        TextField campoSolicitante = new TextField();
-        campoSolicitante.setPromptText("Nome (obs) e telefone");
-        VBox campoSolicitanteBox = new VBox(8);
+        Label valorSolicitante = new Label("Nome (obs) e telefone");
+        VBox campoSolicitanteBox = new VBox();
         campoSolicitanteBox.getChildren().add(labelSolicitante);
-        campoSolicitanteBox.getChildren().add(campoSolicitante);
-        HBox.setHgrow(campoSolicitanteBox, Priority.ALWAYS);
+        campoSolicitanteBox.getChildren().add(valorSolicitante);
 
-        HBox linha3 = new HBox(16, campoDataBox, campoSolicitanteBox);
+        HBox linha3 = new HBox(campoDataBox, campoSolicitanteBox);
 
         Label labelDescricao = new Label("DESCRICAO INICIAL DA OCORRENCIA");
-        TextArea campoDescricao = new TextArea();
-        campoDescricao.setPrefRowCount(3);
-        campoDescricao.setWrapText(true);
-        campoDescricao.setPromptText("Descreve ai o que foi relatado...");
-        VBox campoDescricaoBox = new VBox(8);
+        Label valorDescricao = new Label("Descreve ai o que foi relatado...");
+        VBox campoDescricaoBox = new VBox();
         campoDescricaoBox.getChildren().add(labelDescricao);
-        campoDescricaoBox.getChildren().add(campoDescricao);
+        campoDescricaoBox.getChildren().add(valorDescricao);
 
         card.getChildren().add(tituloCard);
         card.getChildren().add(linha1);
@@ -99,55 +66,29 @@ public class TelaNovaOcorrencia {
         card.getChildren().add(campoDescricaoBox);
 
         HBox linhaBotao = new HBox();
-        linhaBotao.setAlignment(Pos.CENTER_RIGHT);
         Button botaoRegistrar = new Button("Registrar e Despachar");
         linhaBotao.getChildren().add(botaoRegistrar);
 
-        VBox formulario = new VBox(24);
+        VBox formulario = new VBox();
         formulario.getChildren().add(card);
         formulario.getChildren().add(linhaBotao);
-        HBox.setHgrow(formulario, Priority.ALWAYS);
 
-        VBox painel = new VBox(20);
-        painel.setPrefWidth(380);
+        VBox painel = new VBox();
 
         Label tituloViaturas = new Label("VIATURAS DISPONIVEIS");
 
-        Set<String> indisponiveis = Set.of("ASU-01", "ABTR-02");
-
-        ListView<String> lista = new ListView<>();
-        lista.getItems().addAll("ABTR-05", "ASU-12", "AT-03", "UR-03", "ASU-01", "ABTR-02");
-        lista.setCellFactory(lv -> new ListCell<String>() {
-
-            final CheckBox check = new CheckBox();
-            final Label label = new Label();
-            final HBox linha = new HBox(12, check, label);
-
-            {
-                linha.setAlignment(Pos.CENTER_LEFT);
-            }
-
-            @Override
-            protected void updateItem(String item, boolean empty) {
-                super.updateItem(item, empty);
-                if (empty || item == null) {
-                    setGraphic(null);
-                } else {
-                    boolean disponivel = !indisponiveis.contains(item);
-                    check.setDisable(!disponivel);
-                    label.setText(disponivel ? item : item + "  (indisponivel)");
-                    setGraphic(linha);
-                }
-            }
-        });
-        lista.setFixedCellSize(32);
-        lista.prefHeightProperty().bind(lista.fixedCellSizeProperty().multiply(lista.getItems().size()).add(2));
+        VBox listaViaturas = new VBox();
+        listaViaturas.getChildren().add(new Label("ABTR-05"));
+        listaViaturas.getChildren().add(new Label("ASU-12"));
+        listaViaturas.getChildren().add(new Label("AT-03"));
+        listaViaturas.getChildren().add(new Label("UR-03"));
+        listaViaturas.getChildren().add(new Label("ASU-01  (indisponivel)"));
+        listaViaturas.getChildren().add(new Label("ABTR-02  (indisponivel)"));
 
         painel.getChildren().add(tituloViaturas);
-        painel.getChildren().add(lista);
+        painel.getChildren().add(listaViaturas);
 
-        HBox conteudo = new HBox(24, formulario, painel);
-        VBox.setVgrow(conteudo, Priority.ALWAYS);
+        HBox conteudo = new HBox(formulario, painel);
 
         raiz.getChildren().add(conteudo);
 
