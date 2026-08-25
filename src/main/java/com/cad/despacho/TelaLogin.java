ae6250b10e99bc0315f3df99f8bb4d3df88e7a91
Xryonz <i0560008@gmail.com>
2026-08-25 13:21:21 -0300

    login: tirei os campos de texto e senha e os botao de perfil, agora e so label e botao, sem espacamento

--- src/main/java/com/cad/despacho/TelaLogin.java
+++ src/main/java/com/cad/despacho/TelaLogin.java
@@ -2,17 +2,10 @@ package com.cad.despacho;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -24,11 +17,9 @@ public class TelaLogin {
         StackPane raiz = new StackPane();
         raiz.setPrefSize(1200, 750);
 
-        VBox card = new VBox(24);
-        card.setAlignment(Pos.TOP_CENTER);
+        VBox card = new VBox();
 
-        VBox topo = new VBox(16);
-        topo.setAlignment(Pos.CENTER);
+        VBox topo = new VBox();
         StackPane badge = new StackPane();
         Label glifo = new Label("CAD");
         badge.getChildren().add(glifo);
@@ -39,43 +30,20 @@ public class TelaLogin {
         topo.getChildren().add(subtitulo);
 
         Label labelUsuario = new Label("REGISTRO GERAL / CPF");
-        TextField campoRgCpf = new TextField();
-        campoRgCpf.setPromptText("123.456.789-10");
-        VBox boxUsuario = new VBox(8);
-        boxUsuario.getChildren().add(labelUsuario);
-        boxUsuario.getChildren().add(campoRgCpf);
-
         Label labelSenha = new Label("SENHA DE ACESSO TATICO");
-        PasswordField campoSenha = new PasswordField();
-        campoSenha.setPromptText("****************");
-        VBox boxSenha = new VBox(8);
-        boxSenha.getChildren().add(labelSenha);
-        boxSenha.getChildren().add(campoSenha);
 
         Label labelPerfil = new Label("PERFIL DE OPERACAO");
-        ToggleGroup grupoPerfil = new ToggleGroup();
-        ToggleButton botaoDespachante = new ToggleButton("Despachante");
-        botaoDespachante.setToggleGroup(grupoPerfil);
-        botaoDespachante.setMaxWidth(Double.MAX_VALUE);
-        HBox.setHgrow(botaoDespachante, Priority.ALWAYS);
-        botaoDespachante.setSelected(true);
-        ToggleButton botaoSupervisor = new ToggleButton("Supervisor");
-        botaoSupervisor.setToggleGroup(grupoPerfil);
-        botaoSupervisor.setMaxWidth(Double.MAX_VALUE);
-        HBox.setHgrow(botaoSupervisor, Priority.ALWAYS);
+        Button botaoDespachante = new Button("Despachante");
+        Button botaoSupervisor = new Button("Supervisor");
         HBox boxRoles = new HBox(botaoDespachante, botaoSupervisor);
-        VBox boxPerfil = new VBox(8);
-        boxPerfil.getChildren().add(labelPerfil);
-        boxPerfil.getChildren().add(boxRoles);
 
-        VBox formulario = new VBox(20);
-        formulario.getChildren().add(boxUsuario);
-        formulario.getChildren().add(boxSenha);
-        formulario.getChildren().add(boxPerfil);
+        VBox formulario = new VBox();
+        formulario.getChildren().add(labelUsuario);
+        formulario.getChildren().add(labelSenha);
+        formulario.getChildren().add(labelPerfil);
+        formulario.getChildren().add(boxRoles);
 
         Button botaoEntrar = new Button("AUTENTICAR NO CONSOLE");
-        botaoEntrar.setMaxWidth(Double.MAX_VALUE);
-        botaoEntrar.setDefaultButton(true);
         botaoEntrar.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent evento) {
                 TelaPrincipal telaPrincipal = new TelaPrincipal();
